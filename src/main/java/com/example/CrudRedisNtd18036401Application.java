1ecba20c965c035c9dba498c4f8a388d762e219f
dat nguyen <thedatnguyen20@gmail.com>
2021-11-08 10:24:37 +0700

    crud hash

---
+++ src/main/java/com/example/CrudRedisNtd18036401Application.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+@SpringBootApplication
+public class CrudRedisNtd18036401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRedisNtd18036401Application.class, args);
+	}
+	  @Bean
+	    LettuceConnectionFactory jedisConnectionFactory(){
+
+	        return new LettuceConnectionFactory();
+	    }
+
+	    @Bean
+	    RedisTemplate redisTemplate(){
+	        RedisTemplate redisTemplate = new RedisTemplate();
+	        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+	        return redisTemplate;
+	    }
+}
---
+++ src/main/java/com/example/model/Employee.java
@@ -0,0 +1,37 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+    private int id;
+    private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+    
+}
